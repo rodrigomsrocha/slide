0ec5010640661d739e1fae12944cc6cdaa392ac1
Rodrigo Rocha <rodrigomtc82@gmail.com>
2021-03-14 13:41:50 -0300

    style custom controls

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-import { SlideNav } from "./slide.js";
+import SlideNav from "./slide.js";
 
 const slide = new SlideNav(".slide", ".slide-wrapper", "active");
 slide.init();
 slide.addPrevNext(".prev", ".next");
-slide.addControl();
+slide.addControl(".custom-controls");
